6fb22cfeeaf884cc8c279144761313bfdd87318d
111522203011 <22203011@rmd.ac.in>
2025-03-05 14:17:06 +0530

    Final-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './components/Weather.css'
 import App from './App.jsx'
-
+import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
